466c945fbf6f72aabb7bdf37f8ec9ab92e1abb9e
tonglyu <tonglyu9647@gmail.com>
2018-09-30 21:29:13 -0700

    class8 common numbers

--- Class8_1_HashMap/CommonNumbersOfTwoSortedArrays.java
+++ Class8_1_HashMap/CommonNumbersOfTwoSortedArrays.java
@@ -1,3 +1,17 @@
+
+/**
+ * Description Find all numbers that appear in both of two sorted arrays (the
+ * two arrays are all sorted in ascending order).
+ * 
+ * Assumptions In each of the two sorted arrays, there could be duplicate
+ * numbers. Both two arrays are not null.
+ * 
+ * Examples A = {1, 1, 2, 2, 3}, B = {1, 1, 2, 5, 6}, common numbers are [1, 1, 2]
+ * 
+ * Method1: Time = O(m + n), Space = O(1)
+ * Metho
+ */
+
 public class CommonNumbers {
     // Assumption: there could be duplicate in arrays.
     // Method1: two pointers
