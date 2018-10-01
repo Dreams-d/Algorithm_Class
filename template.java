f35d4da1a333e55501f62c11f1c52e7b6154c669
tonglyu <tonglyu9647@gmail.com>
2018-09-30 23:27:16 -0700

    class8 remove certain chars

--- template.java
+++ template.java
@@ -1,5 +1,27 @@
+
 public class Solution {
-    public List<Integer> common(List<Integer> A, List<Integer> B) {
+    // Method1: two pointers
+    public List<Integer> commonI(List<Integer> A, List<Integer> B) {
+        // Assumption: a and b is not null
+        List<Integer> result = new ArrayList<Integer>();
+        int aIdx = 0;
+        int bIdx = 0;
+        while (aIdx < A.size() && bIdx < B.size()) {
+            if (A.get(aIdx) < B.get(bIdx)) {
+                aIdx++;
+            } else if (A.get(aIdx) > B.get(bIdx)) {
+                bIdx++;
+            } else {
+                result.add(A.get(aIdx));
+                aIdx++;
+                bIdx++;
+            }
+        }
+        return result;
+    }
+
+    // Method2: use a hash map
+    public List<Integer> commonI(List<Integer> A, List<Integer> B) {
         List<Integer> result = new ArrayList<Integer>();
         if (A.size() < B.size()) {
             getCommon(A, B, result);
