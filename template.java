a218d095e447dfdc14d1aaf3e2f65b0e835fec58
tonglyu <tonglyu9647@gmail.com>
2018-10-30 23:40:23 -0700

    rev erse string

--- template.java
+++ template.java
@@ -1,4 +1,22 @@
-
+/**
+ * Description
+ * Determine if a small string is a substring of another large string.
+ * Return the index of the first occurrence of the small string in the large string.
+ * Return -1 if the small string is not a substring of the large string.
+ * 
+ * Assumptions
+ * Both large and small are not null
+ * If small is empty string, return 0
+ * 
+ * Examples
+ * “ab” is a substring of “bcabc”, return 2
+ * “bcd” is not a substring of “bcabc”, return -1
+ * "" is substring of "abc", return 0
+ * 
+ * m = large.length(), n = small.length()
+ * Method1: Time = O(m * n), Space = O(1)
+ * Method2: Time = O(n + m - n) = O(m), Space = O(1)
+ */
 public class Solution {
     // Method1: two pointers
     public List<Integer> commonI(List<Integer> A, List<Integer> B) {
