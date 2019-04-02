a38db232441bdf1136ac3297c5b10b536e1ba7ad
tonglyu <tonglyu9647@gmail.com>
2019-04-01 23:22:42 -0700

    finish before permutation of string

--- Class9_StringII/ReverseWordsInASentenceI.java
+++ Class9_StringII/ReverseWordsInASentenceI.java
@@ -9,6 +9,9 @@ package Laicode.Class9_StringII;
  * Examples “I love Google” → “Google love I”
  * 
  * Corner Cases If the given string is null, we do not need to do anything.
+ * 
+ * Time = O(2n)
+ * Space = O(1)
  */
 
 public class ReverseWordsInASentenceI {
@@ -17,24 +20,24 @@ public class ReverseWordsInASentenceI {
     // 2). Therer are no leading or trailing spaces.
     // 3). input is not null
     public String reverseWords(String input) {
-    //try to convert it to char array and
-    //solve the problem in-place
-    char[] array = input.toCharArray();
-    //1. reverse the whole array.
-    reverse(array, 0, array.length - 1);
-    int start = 0;
-    //2. reverse each of the words
-    for (int i = 0; i < array.length; i++) {
-    // the start index of a word
-    if (array[i] != ' ' && (i == 0 || array[i - 1] == ' ')) {
-        start = i;
-    }
-    // the end index of a word
-    if (array[i] != ' ' && (i == array.length - 1 || array[i + 1] == ' ')) {
-        reverse(array, start, i);
-    }
-    }
-    return new String(array);
+        // try to convert it to char array and
+        // solve the problem in-place
+        char[] array = input.toCharArray();
+        // 1. reverse the whole array.
+        reverse(array, 0, array.length - 1);
+        int start = 0;
+        // 2. reverse each of the words
+        for (int i = 0; i < array.length; i++) {
+            // the start index of a word
+            if (array[i] != ' ' && (i == 0 || array[i - 1] == ' ')) {
+                start = i;
+            }
+            // the end index of a word
+            if (array[i] != ' ' && (i == array.length - 1 || array[i + 1] == ' ')) {
+                reverse(array, start, i);
+            }
+        }
+        return new String(array);
     }
 
     private void reverse(char[] array, int left, int right) {
